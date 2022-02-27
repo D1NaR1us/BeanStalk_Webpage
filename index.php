3f7f60ce386c38455224f42426c8081c3223209f
D1NaR1us <Dinar_1994@live.com>
2022-02-27 21:35:39 +0300

    init 2

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         <title>Hello World</title>
     <body bgcolor="gray"></body>
     <Center>
-        <br><font color="red"><h2>Hello World</h2></br></font>
+        <br><font color="yellow"><h2>Hello World</h2></br></font>
     </Center>
 
 
