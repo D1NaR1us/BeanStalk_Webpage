3d7fcb9e4abcf8f3dda92d11ad4d430cda68d6ec
D1NaR1us <Dinar_1994@live.com>
2022-02-27 21:35:14 +0300

    init git repo, add index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<HTML>
+    <HEAD>
+        <title>Hello World</title>
+    <body bgcolor="gray"></body>
+    <Center>
+        <br><font color="red"><h2>Hello World</h2></br></font>
+    </Center>
+
+
+</HTML>
