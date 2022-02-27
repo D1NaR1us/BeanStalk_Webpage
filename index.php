8e6ab7432a7e2a2cf740d4fc02c2711ebee5e846
D1NaR1us <Dinar_1994@live.com>
2022-02-27 23:58:33 +0300

    s

--- index.php
+++ index.php
@@ -5,8 +5,5 @@
     <Center>
         <br><font color="yellow"><h2>Hello World</h2></br></font>
     </Center>
-    <?php
-        echo <br>getHostByName(getHostName()); 
-    ?>
     </body>
 </HTML>
